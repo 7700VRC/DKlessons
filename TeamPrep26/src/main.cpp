24615df52fae7e20cd4c615dd187b0a3109c8bab
George Kirkman <gkirkman@rollingrobots.com>
2026-01-26 10:42:47 -0800

    added motor display

--- TeamPrep26/src/main.cpp
+++ TeamPrep26/src/main.cpp
@@ -4,7 +4,7 @@
 /*    Author:       georgekirkman                                             */
 /*    Created:      1/23/2026, 4:58:45 PM                                     */
 /*    Description:  V5 project                                                */
-/*                                                                            */
+/*    Updated: 1/26/2026. to include motor display                            */
 /*----------------------------------------------------------------------------*/
 
 #include "vex.h"
@@ -15,29 +15,95 @@ using namespace vex;
 competition Competition;
 
 // define your global instances of motors and other devices here
-
+brain Brain;
 controller Controller1;
 
-motor RF=motor(PORT20,ratio18_1,false);
+motor RF = motor(PORT20, ratio18_1, false);
 
-motor LF=motor(PORT12,ratio18_1,true);
-
-
+motor LF = motor(PORT11, ratio18_1, true);
 
 // custom functions
 
-void driveVolts(int lspeed, int rspeed, int wt){
-  lspeed=lspeed*120;
- rspeed=rspeed*120;
-  LF.spin(fwd,lspeed,voltageUnits::mV);
-  
-  RF.spin(fwd,rspeed,voltageUnits::mV);
-  
-  wait(wt,msec);
+void drive(int lspeed, int rspeed, int wt)
+{
+  LF.spin(fwd, lspeed, percent);
+  RF.spin(fwd, rspeed, percent);
+  wait(wt, msec);
 }
 
- 
+void driveVolts(int lspeed, int rspeed, int wt)
+{
+  lspeed = lspeed * 120;
+  rspeed = rspeed * 120;
 
+  LF.spin(fwd, lspeed, voltageUnits::mV);
+  RF.spin(fwd, rspeed, voltageUnits::mV);
+
+  wait(wt, msec);
+}
+
+// Health of the robot functions
+int YOFFSET = 20;
+int NewLineY = 20;
+int LineNumber = 0;
+void MotorDisplay(int y, double curr, double temp)
+{
+  Brain.Screen.setFillColor(transparent);
+  Brain.Screen.printAt(5, YOFFSET + LineNumber * NewLineY, "Current: %.1fA", curr);
+  if (curr < 1)
+    Brain.Screen.setFillColor(green);
+  else if (curr >= 1 && curr <= 2.5)
+    Brain.Screen.setFillColor(yellow);
+  else
+    Brain.Screen.setFillColor(red);
+  Brain.Screen.drawRectangle(140, YOFFSET + LineNumber * NewLineY - 15, 15, 15);
+
+  Brain.Screen.setFillColor(transparent);
+  Brain.Screen.printAt(160, YOFFSET + LineNumber * NewLineY, "Temp: %.1fC", temp);
+  if (temp < 45)
+    Brain.Screen.setFillColor(green);
+  else if (temp <= 50 && temp >= 45)
+    // TRUE and TRUE --> True
+    // TRUE and FALSE --> False
+    // FALSE and FALSE --> False
+    Brain.Screen.setFillColor(yellow);
+  else
+    Brain.Screen.setFillColor(red);
+  Brain.Screen.drawRectangle(275, YOFFSET + LineNumber * NewLineY - 15, 15, 15);
+  Brain.Screen.setFillColor(transparent);
+}
+
+// Displays information on the brain
+void Display()
+{
+  double leftFrontCurr = LF.current(amp);
+  double leftFrontTemp = LF.temperature(celsius);
+
+  double rightFrontCurr = RF.current(amp);
+  double rightFrontTemp = RF.temperature(celsius);
+
+  LineNumber++;
+  if (LF.installed())
+  {
+    MotorDisplay(0, leftFrontCurr, leftFrontTemp);
+    Brain.Screen.printAt(300, YOFFSET + LineNumber * NewLineY, "LeftFront");
+  }
+  else
+   { Brain.Screen.setPenColor(red);
+  Brain.Screen.printAt(5, YOFFSET + LineNumber * NewLineY, "LeftFront Problem");
+  Brain.Screen.setPenColor(white);}
+  LineNumber++;
+  if (RF.installed())
+  {
+    MotorDisplay(60, rightFrontCurr, rightFrontTemp);
+    Brain.Screen.printAt(300, YOFFSET + LineNumber * NewLineY, "RightFront");
+  }
+  else
+   { Brain.Screen.setPenColor(red);
+  Brain.Screen.printAt(5, YOFFSET + LineNumber * NewLineY, "RightFront Problem");
+  Brain.Screen.setPenColor(white);}
+  LineNumber = 0;
+}
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -49,7 +115,8 @@ void driveVolts(int lspeed, int rspeed, int wt){
 /*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
-void pre_auton(void) {
+void pre_auton(void)
+{
 
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
@@ -65,7 +132,8 @@ void pre_auton(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void autonomous(void) {
+void autonomous(void)
+{
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
@@ -81,30 +149,35 @@ void autonomous(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void usercontrol(void) {
+void usercontrol(void)
+{
   // User control code here, inside the loop
-  while (true) {
+  while (true)
+  {
 
-    int ls=Controller1.Axis3.position(pct);
-    int rs=Controller1.Axis2.position(pct);
+    int ls = Controller1.Axis3.position(pct);
+    int rs = Controller1.Axis2.position(pct);
 
-    driveVolts(ls,rs,10);
+    drive(ls, rs, 10);
+    Display();
   }
 }
 
 //
 // Main will set up the competition functions and callbacks.
 //
-int main() {
+int main()
+{
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
 
   // Run the pre-autonomous function.
   pre_auton();
-
+ 
   // Prevent main from exiting with an infinite loop.
-  while (true) {
+  while (true)
+  {
     wait(100, msec);
   }
 }
